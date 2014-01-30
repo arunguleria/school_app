6a10fb5b7274a46c32b22cc38f318910b25d1f28
arun <arunk3106@gmail.com>
2014-01-30 11:56:44 +0530

    Validation testing

--- test/models/photo_test.rb
+++ test/models/photo_test.rb
@@ -5,5 +5,20 @@ class PhotoTest < ActiveSupport::TestCase
   #   assert true
   # end
   
+  test "should not save invalid data" do
+    photo = Photo.new
+    assert photo.invalid?
+    assert_not photo.errors[:filename].empty?
+    assert_not photo.errors[:file_type].empty?
+    assert_not photo.save
+  end
   
+  test "should save the valid data" do
+    photo = Photo.new(filename: "my_photo.jpeg", file_type: "JPEG")
+    assert photo.valid?
+    assert photo.errors[:filename].empty?
+    assert photo.errors[:file_type].empty?
+    assert photo.save
+    
+  end
 end
--- test/models/subjects_teachers_map_test.rb
+++ test/models/subjects_teachers_map_test.rb
@@ -7,5 +7,27 @@ class SubjectsTeachersMapTest < ActiveSupport::TestCase
   
   test "should not save invalid data" do
     map = SubjectsTeachersMap.new
+    assert map.invalid?
+    assert_not map.errors[:teacher].empty?
+    assert_not map.errors[:subject].empty?
+    assert_not map.save
+  end
+  
+  test "should save the valid data" do
+    map = SubjectsTeachersMap.new(subject_id: 3, teacher_id: 3)
+    assert map.valid?
+    assert map.errors[:teacher].empty?
+    assert map.errors[:subject].empty?
+    assert map.save
+  end
+  
+  test "should check for unique teacher_id " do
+    existing_teacher = subjects_teachers_maps(:one)
+    teacher = SubjectsTeachersMap.new(subject_id: existing_teacher.subject_id, teacher_id: existing_teacher.teacher_id)
+    assert_not teacher.valid?
+    assert_not teacher.save
+    assert_not teacher.errors[:teacher_id].empty?
+    assert_equal 1, teacher.errors[:teacher_id].size
+    assert_equal "has already been taken", teacher.errors[:teacher_id].first
   end
 end
